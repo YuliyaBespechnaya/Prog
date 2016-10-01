774db279c4ae477063f2f5577e313daf3e8b08ab
tmp tmp <tmp@rd.mirea.ru>
2016-10-01 15:16:32 +0300

    two. one

--- main.c
+++ main.c
@@ -4,8 +4,8 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	x=-3*4%-6+5; //Произведение -3 и 4 равно -12. Остаток от деления -12 на -6 равно 0. Сумма 0 и 5 равна 5. Программа выведет 5
+    int x;
+	x=-3*4%-6+5; //-3*4=-12. -12%-6=0. 0+5=5. The program will print 5
 	printf("%d", x);
 	return 0;
 }
