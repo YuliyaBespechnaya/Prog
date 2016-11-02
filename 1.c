5d24bb4d119327c1a96ae8ca8a6d5b1793c0bc1f
YuliyaBespechnaya <m_koga@mail.ru>
2016-11-03 02:34:38 +0300

    №1

---
+++ 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    if(a > 0)
+    a = a * 2;
+	else {
+	      if(a < 0)
+	      a = a - 3;
+	      else
+	      a = 10;
+          }
+    printf("a=%d", a);
+    return 0;
+}
